bd6ad1a8d991df5cda531f2bf5bc8133776c3ba8
Carl A. B. Pearson <blank101@gmail.com>
2020-10-14 04:29:34 -0400

    adding fitting assessment dev code

---
+++ ZAF_check.R
@@ -0,0 +1,179 @@
+suppressPackageStartupMessages({
+  require(EpiNow2)
+  require(data.table)
+  require(ggplot2)
+  require(patchwork)
+})
+
+.debug <- "ZAF"
+.args <- if (interactive()) sprintf(c(
+  "cases.rds", "rt_bounds.rds", "%s", "result.png"
+),.debug) else commandArgs(trailingOnly = TRUE)
+
+tariso <- tail(.args, 2)[1]
+
+case.dt <- readRDS(.args[1])[iso3 == tariso][, .(date, confirm = cases )]
+lims.dt <- readRDS(.args[2])[iso3 == tariso]
+
+fill.case <- case.dt[
+  case.dt[, .(date = seq(min(date),max(date),by="day"))],
+  on=.(date),
+  .(date, confirm = fifelse(is.na(confirm), 0, confirm))
+]
+
+generation_time <- as.list(
+  EpiNow2::covid_generation_times[
+    1, .(mean, mean_sd, sd, sd_sd, max=30)
+])
+
+# Set delays between infection and case report
+# (any number of delays can be specifed here)
+incubation_period <- as.list(
+  EpiNow2::covid_incubation_period[
+    1, .(mean, mean_sd, sd, sd_sd, max=30)
+])
+
+reporting_delay <- list(
+  mean = log(6), mean_sd = log(2),
+  sd = log(2), sd_sd = log(1.5),
+  max = 30
+)
+
+# additional time to include for algorithm
+est.window <- 30
+smps <- 1e3
+crs <- 2
+
+early_reported_cases <- case.dt[date <= (lims.dt$interventionend + est.window)]
+with(lims.dt,{
+  early_reported_cases[, era := fifelse(del > 7, "pre", "short")  ]
+  early_reported_cases[date >= transitionstart, era := "window" ]
+  early_reported_cases[date > transitionend, era := "post" ]
+  early_reported_cases[date >= interventionend, era := "censor" ]
+})
+  
+# Add breakpoints
+early_reported_cases[, breakpoint := era %in% c("window", "censor") ]
+  
+  # Run model with breakpoints but otherwise static Rt
+  # This formulation may increase the apparent effect of
+  # the breakpoint but needs to be tested using
+  # model fit criteria (i.e LFO).
+fbkp <- estimate_infections(
+  early_reported_cases, family = "negbin",
+  generation_time = generation_time,
+  delays = list(incubation_period, reporting_delay),
+  samples = smps, warmup = smps*0.1, cores = crs,
+  chains = crs*2,
+  estimate_breakpoints = TRUE,
+  fixed = TRUE, horizon = 0,
+  verbose = TRUE, return_fit = TRUE
+)$samples[variable == "R", .(value), by=.(sample, date)]
+
+nobrk <- estimate_infections(
+  early_reported_cases[,.(date, confirm)], family = "negbin",
+  generation_time = generation_time,
+  delays = list(incubation_period, reporting_delay),
+  samples = smps, cores = crs,
+  chains = crs*2,
+  fixed = FALSE, horizon = 0,
+  verbose = TRUE, return_fit = FALSE
+)$samples[variable == "R", .(value), by=.(sample, date)]
+
+cmbn <- function(brks, non) {
+  res <- rbind(
+    brks[, analysis := "breakpoint"],
+    non[, analysis := "continuous"]
+  )
+  res[, {
+    qs <- quantile(value, probs = c(0.025, 0.25, 0.5, 0.75, 0.975))
+    names(qs) <- c("lo.lo","lo","med","hi","hi.hi")
+    as.list(qs)
+  }, keyby = .(analysis, date)]
+}
+
+
+
+results <- cmbn(fbkp, nobrk)
+
+xscale <- scale_x_date(
+  date_breaks="week", date_labels = c("%b %d"),
+  minor_breaks = NULL
+)
+
+p1 <- function(dt) ggplot(dt) + aes(date) +
+  geom_ribbon(aes(ymin=lo.lo, ymax=hi.hi, fill=analysis), alpha = 0.2) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, fill=analysis), alpha = 0.2) +
+  geom_line(aes(y=med, color=analysis)) +
+  theme_minimal() +
+  theme(
+#    axis.text.x = element_blank(),
+#    axis.title.x = element_blank()
+  ) +
+  xscale +
+  scale_y_continuous("Rt")
+
+p2 <- function(dt) ggplot(dt[date <= (lims.dt$interventionend + est.window)]) + aes(date, confirm) +
+  geom_line() +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = .5)
+  ) +
+  xscale +
+  scale_y_log10("Confirmed Cases")
+
+(p1(results) / p2(fill.case)) & coord_cartesian(
+  xlim = range(re.est$date)
+)
+
+rebreak <- copy(early_reported_cases)
+
+rebreak[era %in% c("window","introduction"), breakpoint := FALSE]
+rebreak[era == "window", era := "post"]
+rebreak[era == "introduction", era := "pre"]
+
+cntr <- as.Date("2020-03-20")
+
+rebreak[
+  between(date, cntr-4, cntr+3),
+  era := "window"
+]
+rebreak[era == "window", breakpoint := TRUE]
+rebreak[cumsum(confirm) <= 10, era := "introduction"]
+rebreak[era == "introduction", breakpoint := TRUE ]
+
+re.est <- estimate_infections(
+  rebreak, family = "negbin",
+  generation_time = generation_time,
+  delays = list(incubation_period, reporting_delay),
+  samples = smps, warmup = smps*0.1, cores = crs,
+  chains = crs*2,
+  estimate_breakpoints = TRUE,
+  fixed = TRUE, horizon = 0,
+  verbose = TRUE, return_fit = TRUE
+)$samples[variable == "R", .(value), by=.(sample, date)]
+
+# 7-centre'd moving average estimate_week_eff
+re.cont <- estimate_infections(
+  rebreak[, .(date, confirm = round(frollmean(confirm, 7, align = "center")))][!is.na(confirm)],
+  family = "negbin",
+  generation_time = generation_time,
+  delays = list(incubation_period, reporting_delay),
+  samples = smps, cores = crs,
+  chains = crs*2,
+  fixed = FALSE, horizon = 0,
+  verbose = TRUE, return_fit = FALSE,
+  estimate_week_eff = FALSE
+)$samples[variable == "R", .(value), by=.(sample, date)]
+
+moreres <- cmbn(re.est, re.cont)
+smooth.cases <- case.dt[, .(date, confirm = round(frollmean(confirm, 7, align = "center")))][!is.na(confirm)]
+
+(p1(moreres) / p2(smooth.cases)) & coord_cartesian(
+  xlim = range(re.est$date)
+)
+
+(p1(results) + p1(moreres) + plot_layout(guides = "collect")) &
+  coord_cartesian(ylim = range(
+    rbind(results, moreres)[, c(lo.lo, hi.hi)]
+))
