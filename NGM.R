da2298e0bae176785840cabdd3b9a3d10c914351
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2020-10-17 16:50:41 +0100

    very minor updates

--- NGM.R
+++ NGM.R
@@ -5,9 +5,14 @@
 
 ngm <- function(
   pops,
-  
+  u_multiplier = 1,
+  cm_reductions = c(0, 0, 0, 0),
+  fIs_reductions = rep(0, pops[[1]]$size)
 ) {
   
+  
+  
+   
 }
 
 saveRDS(ngm, tail(.args, 1))
